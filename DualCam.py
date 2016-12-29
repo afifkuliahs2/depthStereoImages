d20d72795180fe1f8fbfefa761b3194787c030c1
Afif Hendrawan <afif.hendrawan@gmail.com>
2016-12-29 14:43:48 +0700

    get object center

--- DualCam.py
+++ DualCam.py
@@ -14,12 +14,38 @@ class DualCam:
         self.greenUpper = (64,255,255)
         self.pts = deque(maxlen=buffersize)
 
+        self.focalLength = 50
+        self.distance = 100
+
     def makeMask(self, colorMap, rangeLower, rangeUpper, iterationNumber):
         mask = cv2.inRange(colorMap, rangeLower, rangeUpper)
         mask = cv2.erode(mask, None, iterations=iterationNumber)
         mask = cv2.dilate(mask, None, iterations=iterationNumber)
 
         return mask;
+
+    def getCenter(self, contours):
+        c = max(contours, key=cv2.contourArea)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+        return center
+
+    def getDepth(self, c1, c2, init1, init2, frame):
+        center1 = 0
+        center2 = 0
+        l = np.size(frame[0])
+
+
+        if len(c1) > 0 and len(c2) > 0:
+            init1 = self.getCenter(c1)
+            init2 = self.getCenter(c2)
+            center1 = init1[1]
+            center2 = init2[1]
+
+        d = center1 + center2
+
+        return d
 
     def makeConture(self, contours, center, frame):
         if len(contours) > 0:
@@ -28,8 +54,8 @@ class DualCam:
             # centroid
             c = max(contours, key=cv2.contourArea)
             ((x, y), radius) = cv2.minEnclosingCircle(c)
-            M = cv2.moments(c)
-            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            center = self.getCenter(contours)
+            font = cv2.FONT_HERSHEY_SIMPLEX
 
             # only proceed if the radius meets a minimum size
             if radius > 10:
@@ -37,7 +63,8 @@ class DualCam:
                 # then update the list of tracked points
                 cv2.circle(frame, (int(x), int(y)), int(radius),
                            (0, 255, 255), 2)
-                cv2.circle(frame, None, 5, (0, 0, 255), -1)
+                cv2.circle(frame, center, 5, (0, 0, 255), -1) #draw centroid
+                cv2.putText(frame, str(center[1]), (10, 500), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
 
         self.pts.appendleft(center)
         return
@@ -64,6 +91,12 @@ class DualCam:
 
         self.makeConture(cnts, center, img1)
         self.makeConture(cnts2, center2, img2)
+
+        init1 = None
+        init2 = None
+
+        z = self.getDepth(cnts, cnts2, init1, init2, img1)
+        print(str(z))
 
         cv2.imshow('Camera 1', img1)
         cv2.imshow('Camera 2', img2)
